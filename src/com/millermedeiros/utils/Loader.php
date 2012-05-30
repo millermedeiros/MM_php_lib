949fe3b998a3a61d67dcff9d1f6b76a1842da3cf
millermedeiros <miller@millermedeiros.com>
2012-05-30 10:36:13 -0300

    update path utils and loader to latest.

--- src/com/millermedeiros/utils/Loader.php
+++ src/com/millermedeiros/utils/Loader.php
@@ -3,52 +3,72 @@
 /**
  * Helper Class to load (include) external files [static class]
  * @author Miller Medeiros
- * @version 0.5 (2010/05/28)
+ * @version 0.8.0 (2011/10/19)
  */
 class Loader {
 
     private function __construct() {}
-	
-	/**
-	 * Load external file and assign variables
-	 * @param string $file_path	Path to the desired file
-	 * @param array $data [optional]	Array or Object with data that should be passed to the loaded file [ex: $data = array('foo' => 'lorem', 'bar' => 'ipsum')]
-	 */
-	public static function load($file_path, $data = NULL){
-		if(isset($data)){
-			foreach($data as $key=>$value){
-				$$key = $value; //set variables inside this method scope (same scope as the included file)
-			}
-		}
-		include $file_path;
-	}
-	
-	/**
-	 * Load Simple Template File
-	 * - only replace variables wrapped in '::' with the $data item with same key.
-	 * - if you need loops and conditionals just use regular PHP and set '$execute_php' to TRUE.
-	 * @param string $file_path	Path to the desired file
-	 * @param array $data [optional]	Array or Object with data that should be passed to the loaded file [ex: $data = array('foo' => 'lorem', 'bar' => 'ipsum')]
-	 * @param bool	$execute_php [optional]	If PHP code inside template should be executed.
-	 */
-	public static function loadTemplate($file_path, $data = NULL, $execute_php = FALSE){
-		if($execute_php){
-			ob_start();
-			self::load($file_path, $data);
-			$output = ob_get_clean();
-		}else{
-			$output = file_get_contents($file_path);	
-		}
-		
-		if(isset($data)){
-			foreach($data as $key=>$value){
-				if(is_string($value)){
-					$output = preg_replace("/::$key::/", $value, $output); //replace everything between '::' with the proper value
-				}
-			}
-		}
-		echo $output;
-	}
-	
+
+
+    /**
+     * Load external file and assign variables
+     * @param string $file_path Path to the desired file
+     * @param array $data [optional]    Array or Object with data that should be passed to the loaded file [ex: $data = array('foo' => 'lorem', 'bar' => 'ipsum')]
+     */
+    public static function load($file_path, $data = NULL){
+        if(isset($data)){
+            foreach($data as $key=>$value){
+                $$key = $value; //set variables inside this method scope (same scope as the included file)
+            }
+        }
+        include $file_path;
+    }
+
+    /**
+     * Load Simple Template File
+     * - replace variables wrapped in '{{}}' with the $data item with same key or
+     * global variable.
+     * - if you need loops and conditionals just use regular PHP and set '$execute_php' to TRUE
+     * (default behavior).
+     * - useful since `<?= ?>` can be disabled if `short_open_tag` is disabled (PHP < 5.4)
+     * @param string $file_path Path to the desired file
+     * @param array $data [optional]    Array or Object with data that should be passed to the loaded file [ex: $data = array('foo' => 'lorem', 'bar' => 'ipsum')]
+     * @param bool  $execute_php [optional] If PHP code inside template should be executed.
+     * @param bool  $escape_html [optional] If strings should be html escaped.
+     */
+    public static function loadTemplate($file_path, $data = NULL, $execute_php = TRUE, $escape_html = TRUE){
+        if($execute_php){
+            ob_start();
+            self::load($file_path, $data);
+            $output = ob_get_clean();
+        }else{
+            $output = file_get_contents($file_path);
+        }
+
+        $n_matches = preg_match_all('/\{\{([\-\_\w\d]+)\}\}/', $output, $keys);
+        $data = (array) $data; //expect data to be an array for replacements
+        $val;
+
+        if($n_matches){
+            foreach($keys[1] as $key){
+                if( isset($data[$key]) && ( is_string($data[$key]) || is_numeric($data[$key]) ) ){
+                    $val = $data[$key];
+                } else if ( isset($$key) && ( is_string($$key) || is_numeric($$key) ) ) {
+                    $val = $$key;
+                } else if ( @constant($key) ) {
+                    $val = constant($key);
+                } else {
+                    $val = '';
+                }
+                if($escape_html){
+                    $val = htmlspecialchars($val);
+                }
+                $output = preg_replace("/\{\{$key\}\}/", $val, $output);
+            }
+        }
+
+        echo $output;
+    }
+
 }
 ?>
--- src/com/millermedeiros/utils/PathUtils.php
+++ src/com/millermedeiros/utils/PathUtils.php
@@ -2,67 +2,69 @@
 /**
  * Path utilities
  * @author Miller Medeiros
- * @version 0.1 (2010/02/23)
+ * @version 0.2.1 (2012/05/30)
  */
 class PathUtils {
 
-	/**
-	 * Constructor - Static Class
-	 * @private
-	 */
-	private function __construct(){}
-	
-	/**
-	 * Remove all duplicated '/' except after ':' (because of "http://" and "file:///") and convert all '\' to '/'
-	 * @param string $path Path or URL
-	 * @return string Formated string
-	 */
-	public static function sanitizePath($path){
-		$output = '';
-		$output = preg_replace('/(?<!:)\/{2,}|\\\\+/', '/', $path); //replace '//' and '\+' with '/' (unless '//' comes after ':')
-		$output = preg_replace('/^\//', '', $output); //remove '/' from the beginning of the string
-		return $output;
-	}
-	
-	/**
-	 * Convert relative path into canonicalized absolute pathname (calls PHP realpath method + PathUtils::sanitizePath).
-	 * @param string $path	Path to be checked.
-	 * @return string|bool	Canonicalized absolute pathname or FALSE on failure, e.g. if file doesn't exist.
-	 */
-	public static function toRealPath($path){
-		$real_path = '';
-		$real_path = realpath($path);
-		if(! $real_path){ //realpath() returns FALSE if path doesn't exist
-			return FALSE;
-		}
-		$real_path = self::sanitizePath($real_path);
-		return $real_path;
-	}
-	
-	/**
-	 * Get relative path to a parent folder.
-	 * - ex: StringUtils::getRelativeRoot('/lorem/ipsum/dolor/', '/lorem/') -> returns '../../'. 
-	 * @param string $current_folder [optional]	Current folder path (ex: '/lorem/ipsum/dolor') uses $_SERVER['REQUEST_URI'] by default.
-	 * @param string $base_folder [optional]	Root folder path (ex: '/lorem/').
-	 * @return string	Relative path to the $base_folder.
-	 */
-	public static function getRelativeRoot($current_folder = NULL, $base_folder = ''){
-		$relative_root = '';
-		
-		$current_folder = (isset($current_folder))? $current_folder : @$_SERVER['REQUEST_URI']; // @ suppress errors if REQUEST_URI doesn't exist
-		
-		$current_folder = str_replace($base_folder, '', $current_folder);
-		$current_folder = preg_replace('/^\/|\/$/', '', $current_folder); // removes '/' from beginning and end of string
-		
-		if(!$current_folder || $current_folder == '/' || $current_folder == './'){
-			$relative_root = './';
-		} else if(preg_match('/^(\.\.\/)+$/', $current_folder)){ // check if folder is already a relative path to root '../+' 
-			$relative_root = $current_folder;
-		} else{
-			$relative_root = preg_replace('/[^\/]+\/?/', '../', $current_folder); // add '../' for each path depth
-		}
-		return $relative_root;
-	}
-	
+    /**
+     * Constructor - Static Class
+     * @private
+     */
+    private function __construct(){}
+
+    /**
+     * Remove all duplicated '/' except after ':' (because of "http://" and "file:///") and convert all '\' to '/'
+     * @param string $path Path or URL
+     * @return string Formated string
+     */
+    public static function sanitizePath($path){
+        $output = '';
+        $output = preg_replace('/(?<!:)\/{2,}|\\\\+/', '/', $path); //replace '//' and '\+' with '/' (unless '//' comes after ':')
+        return $output;
+    }
+
+    /**
+     * Convert relative path into canonicalized absolute pathname (calls PHP realpath method + PathUtils::sanitizePath).
+     * @param string $path  Path to be checked.
+     * @return string|bool  Canonicalized absolute pathname or FALSE on failure, e.g. if file doesn't exist.
+     */
+    public static function toRealPath($path){
+        $real_path = '';
+        $real_path = realpath($path);
+        if(! $real_path){ //realpath() returns FALSE if path doesn't exist
+            return FALSE;
+        }
+        $real_path = self::sanitizePath($real_path);
+        return $real_path;
+    }
+
+    /**
+     * Get relative path to a parent folder.
+     * - ex: StringUtils::getRelativeRoot('/lorem/ipsum/dolor/', '/lorem/') -> returns '../../'
+     * - ex: StringUtils::getRelativeRoot('/lorem/ipsum/dolor', '/lorem/') -> returns '../'
+     * @param string $current_folder [optional] Current folder path (ex: '/lorem/ipsum/dolor') uses $_SERVER['REQUEST_URI'] by default.
+     * @param string $base_folder [optional]    Root folder path (ex: '/lorem/').
+     * @return string   Relative path to the $base_folder.
+     */
+    public static function getRelativeRoot($current_folder = NULL, $base_folder = ''){
+        $relative_root = '';
+
+        $current_folder = (isset($current_folder))? $current_folder : @$_SERVER['REQUEST_URI']; // @ suppress errors if REQUEST_URI doesn't exist
+
+        $current_folder = str_replace($base_folder, '', $current_folder);
+        $current_folder = preg_replace('/^\/$/', '', $current_folder); // removes '/' from beginning of string
+
+        if(!$current_folder || $current_folder == '/' || $current_folder == './' || !preg_match('/\//', $current_folder)){
+            // check if it is at the root
+            $relative_root = './';
+        } else if(preg_match('/^(\.\.\/)+$/', $current_folder)){ // check if folder is already a relative path to root '../+'
+            $relative_root = $current_folder;
+        } else{
+            $relative_root = preg_replace('/[^\/]+\//', '../', $current_folder); // add '../' for each path depth
+            $relative_root = preg_replace('/[^\/\.]+/', '', $relative_root); // remove non-relative paths
+        }
+        return $relative_root;
+    }
+
 }
 ?>
